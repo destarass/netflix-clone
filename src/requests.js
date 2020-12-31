1e77401cea248631764e627b728de077844168d3
DestaraSyanandi <destasyanando@gmail.com>
2020-12-31 10:33:18 +0700

    push project netflix-clone

---
+++ src/requests.js
@@ -0,0 +1,15 @@
+const API_KEY = "d7d22c83089d622bf9fc6dcd4347060a"; 
+
+const requests= {
+    fetchNetflix0riginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+
+}
+export default requests
+
